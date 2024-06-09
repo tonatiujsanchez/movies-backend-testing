7155ed248c635f7a256ce15e799d540bcca543cc
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-06-09 14:51:06 -0600

    controllers, models y routes, añadidos

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Actor = require('./Actor')
+const Director = require('./Director')
+const Genre = require('./Genre')
+const Movie = require('./Movie')
+
+Movie.belongsToMany(Genre, { through: 'movieGenres' })
+Genre.belongsToMany(Movie, { through: 'movieGenres' })
+
+Movie.belongsToMany(Actor, { through: 'movieActors' })
+Genre.belongsToMany(Movie, { through: 'movieActors' })
+
+Movie.belongsToMany(Director, { through: 'movieDirectors' })
+Director.belongsToMany(Movie, { through: 'movieDirectors' })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const routerActor = require('./actor.router')
+const routerDirector = require('./director.router')
+const routerGenre = require('./genre.router')
+const routerMovie = require('./movie.router')
+
 
 // colocar las rutas aquí
+router.use('/genres', routerGenre)
+router.use('/actors', routerActor)
+router.use('/directors', routerDirector)
+router.use('/movies', routerMovie)
 
-
-module.exports = router;
+module.exports = router
